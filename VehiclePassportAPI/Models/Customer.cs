5afc53d022fc94a6858164b6595c795a312309e8
WedithaV <wedithav@gmail.com>
2025-05-07 23:03:23 +0530

    create models- Customer/Vehicle

---
+++ VehiclePassportAPI/Models/Customer.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VehiclePassportAPI.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last name is required.")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password hash is required.")]
+        [StringLength(16, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 16 characters.")]
+        public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "Phone number must be exactly 10 digits.")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Phone number must contain exactly 10 digits.")]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "NIC is required.")]
+        [StringLength(12, MinimumLength = 12, ErrorMessage = "NIC must have 12 characters")]
+        public string NIC { get; set; } = string.Empty;
+
+        [Range(0, int.MaxValue, ErrorMessage = "Loyalty points cannot be negative.")]
+        public int LoyaltyPoints { get; set; }
+
+        [Url(ErrorMessage = "Invalid URL format for profile picture.")]
+        public string ProfilePicture { get; set; } = string.Empty;
+
+        public List<Vehicle> Vehicles { get; set; } = null!;
+    }
+}
---
+++ VehiclePassportAPI/Models/Vehicle.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VehiclePassportAPI.Models
+{
+    public class Vehicle
+    {
+        [Key]
+        public int VehicleID { get; set; }
+
+        [Required(ErrorMessage = "Registration number is required.")]
+        [StringLength(8, MinimumLength = 8, ErrorMessage = "Registration number must be exactly 8 characters.")]
+        public string RegistrationNumber { get; set; } = string.Empty;
+
+        [Range(0, int.MaxValue, ErrorMessage = "Mileage cannot be negative.")]
+        public int Mileage { get; set; }
+
+        [Required(ErrorMessage = "Fuel type is required.")]
+        public string FuelType { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Chassis number is required.")]
+        [StringLength(17, MinimumLength = 17, ErrorMessage = "Chassis number must be exactly 17 characters.")]
+        public string ChassiNumber { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Brand is required.")]
+        public string Brand { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Model is required.")]
+        public string Model { get; set; } = string.Empty;
+
+        [ForeignKey("Customer")]
+        public int CustomerID { get; set; }
+
+        public Customer Customer { get; set; } = null!;
+    }
+}
